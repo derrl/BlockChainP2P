206734e3629890660a83f950279482eeb27a9bc1
derrl <lhzsdu@gmail.com>
2019-03-18 18:12:06 +0000

    Add some supports in include dir

---
+++ include/rpclib/cmake-build-debug/CMakeFiles/rpc.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "F:/learning/iit/cs550/blockchain_system/rpclib/dependencies/src/format.cc" "F:/learning/iit/cs550/blockchain_system/rpclib/cmake-build-debug/CMakeFiles/rpc.dir/dependencies/src/format.cc.obj"
+  "F:/learning/iit/cs550/blockchain_system/rpclib/dependencies/src/posix.cc" "F:/learning/iit/cs550/blockchain_system/rpclib/cmake-build-debug/CMakeFiles/rpc.dir/dependencies/src/posix.cc.obj"
+  "F:/learning/iit/cs550/blockchain_system/rpclib/lib/rpc/client.cc" "F:/learning/iit/cs550/blockchain_system/rpclib/cmake-build-debug/CMakeFiles/rpc.dir/lib/rpc/client.cc.obj"
+  "F:/learning/iit/cs550/blockchain_system/rpclib/lib/rpc/detail/client_error.cc" "F:/learning/iit/cs550/blockchain_system/rpclib/cmake-build-debug/CMakeFiles/rpc.dir/lib/rpc/detail/client_error.cc.obj"
+  "F:/learning/iit/cs550/blockchain_system/rpclib/lib/rpc/detail/response.cc" "F:/learning/iit/cs550/blockchain_system/rpclib/cmake-build-debug/CMakeFiles/rpc.dir/lib/rpc/detail/response.cc.obj"
+  "F:/learning/iit/cs550/blockchain_system/rpclib/lib/rpc/detail/server_session.cc" "F:/learning/iit/cs550/blockchain_system/rpclib/cmake-build-debug/CMakeFiles/rpc.dir/lib/rpc/detail/server_session.cc.obj"
+  "F:/learning/iit/cs550/blockchain_system/rpclib/lib/rpc/dispatcher.cc" "F:/learning/iit/cs550/blockchain_system/rpclib/cmake-build-debug/CMakeFiles/rpc.dir/lib/rpc/dispatcher.cc.obj"
+  "F:/learning/iit/cs550/blockchain_system/rpclib/lib/rpc/nonstd/optional.cc" "F:/learning/iit/cs550/blockchain_system/rpclib/cmake-build-debug/CMakeFiles/rpc.dir/lib/rpc/nonstd/optional.cc.obj"
+  "F:/learning/iit/cs550/blockchain_system/rpclib/lib/rpc/rpc_error.cc" "F:/learning/iit/cs550/blockchain_system/rpclib/cmake-build-debug/CMakeFiles/rpc.dir/lib/rpc/rpc_error.cc.obj"
+  "F:/learning/iit/cs550/blockchain_system/rpclib/lib/rpc/server.cc" "F:/learning/iit/cs550/blockchain_system/rpclib/cmake-build-debug/CMakeFiles/rpc.dir/lib/rpc/server.cc.obj"
+  "F:/learning/iit/cs550/blockchain_system/rpclib/lib/rpc/this_handler.cc" "F:/learning/iit/cs550/blockchain_system/rpclib/cmake-build-debug/CMakeFiles/rpc.dir/lib/rpc/this_handler.cc.obj"
+  "F:/learning/iit/cs550/blockchain_system/rpclib/lib/rpc/this_server.cc" "F:/learning/iit/cs550/blockchain_system/rpclib/cmake-build-debug/CMakeFiles/rpc.dir/lib/rpc/this_server.cc.obj"
+  "F:/learning/iit/cs550/blockchain_system/rpclib/lib/rpc/this_session.cc" "F:/learning/iit/cs550/blockchain_system/rpclib/cmake-build-debug/CMakeFiles/rpc.dir/lib/rpc/this_session.cc.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ASIO_STANDALONE"
+  "RPCLIB_ASIO=clmdep_asio"
+  "RPCLIB_FMT=clmdep_fmt"
+  "RPCLIB_MSGPACK=clmdep_msgpack"
+  "RPCLIB_WIN32"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../dependencies/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ include/rpclib/cmake-build-debug/CMakeFiles/rpc.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rpc.dir/lib/rpc/dispatcher.cc.obj"
+  "CMakeFiles/rpc.dir/lib/rpc/server.cc.obj"
+  "CMakeFiles/rpc.dir/lib/rpc/client.cc.obj"
+  "CMakeFiles/rpc.dir/lib/rpc/this_handler.cc.obj"
+  "CMakeFiles/rpc.dir/lib/rpc/this_session.cc.obj"
+  "CMakeFiles/rpc.dir/lib/rpc/this_server.cc.obj"
+  "CMakeFiles/rpc.dir/lib/rpc/rpc_error.cc.obj"
+  "CMakeFiles/rpc.dir/lib/rpc/detail/server_session.cc.obj"
+  "CMakeFiles/rpc.dir/lib/rpc/detail/response.cc.obj"
+  "CMakeFiles/rpc.dir/lib/rpc/detail/client_error.cc.obj"
+  "CMakeFiles/rpc.dir/lib/rpc/nonstd/optional.cc.obj"
+  "CMakeFiles/rpc.dir/dependencies/src/format.cc.obj"
+  "CMakeFiles/rpc.dir/dependencies/src/posix.cc.obj"
+  "librpc.pdb"
+  "librpc.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rpc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
